249863523e782843eb4d5e40fb8355bcfdee45f8
Krechetro <87053070+Krechetro@users.noreply.github.com>
2021-12-30 13:36:01 +0300

    Add files via upload

---
+++ MyCRUD/MyCRUD/src/main/java/Web/controller/UserController.java
@@ -0,0 +1,63 @@
+package Web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import Web.model.User;
+import Web.service.UserService;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String getUsers(Model model) {
+        model.addAttribute("users", userService.getAllUser());
+        return "index";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "new";
+    }
+
+    @PostMapping()
+    public String createUser(@ModelAttribute("user") User user, BindingResult bindingResult) {
+        userService.createUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editUser(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateUser(@ModelAttribute("user") User user, BindingResult bindingResult,
+                             @PathVariable("id") int id) {
+        userService.updateUser(id, user);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") int id) {
+        userService.deleteUser(id);
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/{id}")
+    public String getUserById(@PathVariable int id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "user";
+    }
+}
